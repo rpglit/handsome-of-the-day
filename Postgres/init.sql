55355eb6321e92f8619a56326e98a2b27ebd9fe0
rpglit <87199237+rpglit@users.noreply.github.com>
2024-09-18 12:06:51 +0300

    Update init.sql

--- Postgres/init.sql
+++ Postgres/init.sql
@@ -1,12 +1,12 @@
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(30) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
-    pidrs INT DEFAULT 0,
+    losers INT DEFAULT 0,
     nices INT DEFAULT 0
 );
 
 CREATE TABLE IF NOT EXISTS logs (
     date DATE PRIMARY KEY,
-    pidr INT REFERENCES users(id),
+    loser INT REFERENCES users(id),
     nice INT REFERENCES users(id)
 );
