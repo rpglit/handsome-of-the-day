85c91d0e6d85399eee701d1b7a888d3e81493e91
rpglit <87199237+rpglit@users.noreply.github.com>
2024-09-19 20:13:40 +0300

    Update init.sql

--- Postgres/init.sql
+++ Postgres/init.sql
@@ -1,3 +1,15 @@
+-- create-db.sql
+
+-- Создание базы данных
+CREATE DATABASE bot_database;
+
+-- Создание пользователя
+CREATE USER bot_user WITH PASSWORD 'password';
+
+-- Назначение привилегий
+GRANT ALL PRIVILEGES ON DATABASE bot_database TO bot_user;
+
+
 CREATE TABLE IF NOT EXISTS users (
     id VARCHAR(30) PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
